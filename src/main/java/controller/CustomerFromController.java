0cb71e32ef6a0b8f415d9156baf0267b50c360f8
AkashHirumal <akashhirumal@gmail.com>
2024-12-28 11:27:19 +0530

    Customer_form

---
+++ src/main/java/controller/CustomerFromController.java
@@ -0,0 +1,119 @@
+package controller;
+
+import model.Customer;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.net.URL;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class CustomerFromController implements Initializable {
+
+    @FXML
+    private TableColumn<?, ?> colId;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private TableColumn<?, ?> colAddress;
+
+    @FXML
+    private TableColumn<?, ?> colSalary;
+
+    @FXML
+    private TableView<Customer> tblCustomers;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtSalary;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+        customerList.add(new Customer(
+                txtId.getText(),
+                txtName.getText(),
+                txtAddress.getText(),
+                Double.parseDouble(txtSalary.getText())
+        ));
+        loadTable();
+    }
+
+    @FXML
+    void btnRemoveOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnViewItemFormOnAction(ActionEvent event) {
+
+    }
+
+    List<Customer> customerList = new ArrayList<>();
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
+        colSalary.setCellValueFactory(new PropertyValueFactory<>("salary"));
+        loadTable();
+    }
+
+    private void loadTable() {
+
+        try {
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/thogakade",
+                    "root",
+                    "12345678");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT *FROM customer");
+            while (resultSet.next()){
+                customerList.add(new Customer(
+                                resultSet.getString(1),
+                                resultSet.getString(2),
+                                resultSet.getString(3),
+                                resultSet.getDouble(4)
+                        )
+                );
+            }
+
+
+            System.out.println(connection);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        ObservableList<Customer> customerObservableList = FXCollections.observableArrayList();
+
+        customerList.forEach(customer -> {
+            customerObservableList.add(customer);
+        });
+
+
+        tblCustomers.setItems(customerObservableList);
+    }
+}
---
+++ src/main/java/model/Item.java
@@ -0,0 +1,18 @@
+package model;
+
+import lombok.*;
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class Item {
+
+    private String id;
+    private String name;
+    private Integer quantity;
+    private Double price;
+    private String description;
+
+
+
+}
